c51ffffacbd2f4805864c764eb905ba83b545113
Igor Macedo <imacedo2000.im@gmail.com>
2021-01-27 17:20:38 -0300

    Inserção de algumas perguntas. Aula 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,17 +24,38 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Quiz da Quebrada</title>
+        <meta name="title" content="Quiz da Quebrada" />
+        <meta name="description" content="Teste os seus conhecimentos sobre algumas gírias da quebrada e tente, da próxima vez, não ficar perdido quando alguém mandar um salve para você." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Quiz da Quebrada" />
+        <meta property="og:description" content="Teste os seus conhecimentos sobre algumas gírias da quebrada e tente, da próxima vez, não ficar perdido quando alguém mandar um salve para você." />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Quiz da Quebrada" />
+        <meta property="twitter:description" content="Teste os seus conhecimentos sobre algumas gírias da quebrada e tente, da próxima vez, não ficar perdido quando alguém mandar um salve para você." />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Component {...pageProps} />
+        <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,62 @@
-import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
-//const BackgroundImage = styled.div`
+// const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
 //   flex: 1;
 //   background-size: cover;
 //   background-position: center;
-//`
-
-export const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
-    margin: auto 10%;
-    @media screen and (max-width: 500px) {
-        margin: auto;
-        padding: 15px;
-    } 
-`
+// `
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-      <QuizBackground backgroundImage={db.bg}>
-          <QuizContainer>
-              <Widget>
-                  <Widget.Header>
-                      <h1>{ db.title }</h1>
-                  </Widget.Header>
-                  <Widget.Content>
-                      <p>{ db.description }</p>
-                  </Widget.Content>
-              </Widget>
-              <Widget>
-                  <Widget.Content>
-                      <h1>Trampo da rapaziada</h1>
-                  </Widget.Content>
-              </Widget>
-              <Footer/>
-          </QuizContainer>
-          <GitHubCorner projectUrl=""/>
-      </QuizBackground>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{ db.title }</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{ db.description }</p>
+            <form onSubmit={(infosDoEventos) => {
+              infosDoEventos.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                onChange={(infosDoEvento) => {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Digite seu nome"
+                name="nomeDoUsuario"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar como ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Trampo da rapaziada</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/Imacedoo/quiz-da-quebrada" />
+    </QuizBackground>
   );
 }
